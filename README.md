5bef78b16d415fe48a8a67193441c29e78374b29
ggunning <garethgunning@gmail.com>
2020-02-20 00:11:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ftp-demo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ftp-demo)
---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { FormsModule } from '@angular/forms';
+
+import { MaterialModule } from './material.module';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule, MaterialModule  ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
